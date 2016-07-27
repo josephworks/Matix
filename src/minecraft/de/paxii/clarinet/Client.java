b5d875b49c0768fca192759875b552cd86dcdb54
Paxi <dev@paxii.de>
2016-07-27 21:11:39 +0200

    [RELEASE] Release Version 1.6.1B

--- src/minecraft/de/paxii/clarinet/Client.java
+++ src/minecraft/de/paxii/clarinet/Client.java
@@ -29,11 +29,11 @@ public class Client {
 	@Getter
 	private static final String clientName = "Matix";
 	@Getter
-	private static final String clientVersion = "1.6.0B";
+	private static final String clientVersion = "1.6.1B";
 	@Getter
 	private static final String gameVersion = "1.10";
 	@Getter
-	private static final int clientBuild = 16004;
+	private static final int clientBuild = 16005;
 	@Getter
 	private static final String clientURL = "http://paxii.de/Matix/";
 
