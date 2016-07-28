faef8e7e294316e1564fa3fd58d0465a1120f624
Paxi <dev@paxii.de>
2016-07-28 23:01:11 +0200

    [TASK] Add PlayerUpdateWalkingEvent

---
+++ src/minecraft/de/paxii/clarinet/event/events/player/PlayerUpdateWalkingEvent.java
@@ -0,0 +1,9 @@
+package de.paxii.clarinet.event.events.player;
+
+import de.paxii.clarinet.event.events.type.EventCancellable;
+
+/**
+ * Created by Lars on 28.07.2016.
+ */
+public class PlayerUpdateWalkingEvent extends EventCancellable {
+}
