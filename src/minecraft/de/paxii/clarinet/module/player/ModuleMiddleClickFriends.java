c1bc67b98444fc30a3cc4440d1ca577899efd23d
Paxi <dev@paxii.de>
2016-08-01 22:55:32 +0200

    [BUGFIX] Fix MiddleClick click registering
    Now using KeyPressedEvent

--- src/minecraft/de/paxii/clarinet/module/player/ModuleMiddleClickFriends.java
+++ src/minecraft/de/paxii/clarinet/module/player/ModuleMiddleClickFriends.java
@@ -3,6 +3,7 @@ package de.paxii.clarinet.module.player;
 import de.paxii.clarinet.Wrapper;
 import de.paxii.clarinet.event.EventHandler;
 import de.paxii.clarinet.event.events.game.IngameTickEvent;
+import de.paxii.clarinet.event.events.game.KeyPressedEvent;
 import de.paxii.clarinet.module.Module;
 import de.paxii.clarinet.module.ModuleCategory;
 import de.paxii.clarinet.util.chat.Chat;
@@ -28,31 +29,23 @@ public class ModuleMiddleClickFriends extends Module {
 	}
 
 	@EventHandler
-	public void onTick(IngameTickEvent event) {
-		this.timeManager.updateTimer();
+	public void onKeyPress(KeyPressedEvent event) {
+		if (event.getKey() == -98) {
+			Entity pointedEntity = Wrapper.getMinecraft().pointedEntity;
 
-		if (this.timeManager.sleep(500L)) {
-			if (Mouse.isButtonDown(2)) {
+			if (pointedEntity != null) {
+				if (pointedEntity instanceof EntityPlayer) {
+					String userName = pointedEntity.getName();
 
-				Entity pointedEntity = Wrapper.getMinecraft().pointedEntity;
-
-				if (pointedEntity != null) {
-					if (pointedEntity instanceof EntityPlayer) {
-						String userName = pointedEntity.getName();
-
-						if (!Wrapper.getFriendManager().isFriend(userName)) {
-							Wrapper.getFriendManager().addFriend(userName);
-							Chat.printClientMessage("Added Friend " + userName + ".");
-						} else {
-							Wrapper.getFriendManager().removeFriend(userName);
-							Chat.printClientMessage("Removed Friend " + userName + ".");
-						}
+					if (!Wrapper.getFriendManager().isFriend(userName)) {
+						Wrapper.getFriendManager().addFriend(userName);
+						Chat.printClientMessage("Added Friend " + userName + ".");
+					} else {
+						Wrapper.getFriendManager().removeFriend(userName);
+						Chat.printClientMessage("Removed Friend " + userName + ".");
 					}
 				}
-
 			}
-
-			this.timeManager.updateLast();
 		}
 	}
 
