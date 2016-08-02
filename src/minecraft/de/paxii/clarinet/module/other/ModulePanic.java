b2d0ea69d8f7ecfa29331559e0620970e0e9eb51
Paxi <dev@paxii.de>
2016-08-02 22:43:44 +0200

    [TASK] Restore disabled modules after disabling panic

--- src/minecraft/de/paxii/clarinet/module/other/ModulePanic.java
+++ src/minecraft/de/paxii/clarinet/module/other/ModulePanic.java
@@ -4,22 +4,40 @@ import de.paxii.clarinet.Wrapper;
 import de.paxii.clarinet.module.Module;
 import de.paxii.clarinet.module.ModuleCategory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Lars on 20.03.2016.
  */
 public class ModulePanic extends Module {
+	private ArrayList<Module> enabledModules;
+
 	public ModulePanic() {
 		super("Panic", ModuleCategory.OTHER);
+
+		this.enabledModules = new ArrayList<>();
 
 		this.setDescription("Turns all modules off.");
 	}
 
 	@Override
 	public void onEnable() {
-		Wrapper.getModuleManager().getModuleList().forEach((moduleName, module) -> {
-			if (module.isEnabled()) {
+		this.enabledModules.clear();
+
+		Wrapper.getModuleManager().getModuleList().values().forEach(module -> {
+			if (module.isEnabled() && module != this) {
+				this.enabledModules.add(module);
 				module.setEnabled(false);
 			}
 		});
+	}
+
+	@Override
+	public void onDisable() {
+		for (Module module : this.enabledModules) {
+			module.setEnabled(true);
+		}
 	}
 }
