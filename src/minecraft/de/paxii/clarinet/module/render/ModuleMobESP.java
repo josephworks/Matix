4e914a28de5e8ba9f75ca62b617555a424f283b5
Paxi <dev@paxii.de>
2017-01-11 00:15:10 +0100

    [TASK] Refactor ESPs

--- src/minecraft/de/paxii/clarinet/module/render/ModuleMobESP.java
+++ src/minecraft/de/paxii/clarinet/module/render/ModuleMobESP.java
@@ -1,8 +1,7 @@
 package de.paxii.clarinet.module.render;
 
-import de.paxii.clarinet.Wrapper;
 import de.paxii.clarinet.event.EventHandler;
-import de.paxii.clarinet.event.events.entity.PreRenderEntityEvent;
+import de.paxii.clarinet.event.events.entity.PostRenderEntityEvent;
 import de.paxii.clarinet.module.Module;
 import de.paxii.clarinet.module.ModuleCategory;
 import de.paxii.clarinet.util.render.GL11Helper;
@@ -17,6 +16,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import org.lwjgl.opengl.GL11;
 
 public class ModuleMobESP extends Module {
+
   public ModuleMobESP() {
     super("MobESP", ModuleCategory.RENDER, -1);
 
@@ -25,29 +25,12 @@ public class ModuleMobESP extends Module {
   }
 
   @EventHandler
-  public void renderESP(PreRenderEntityEvent event) {
+  public void renderESP(PostRenderEntityEvent event) {
     Entity entity = event.getRenderedEntity();
 
-    if (!(entity instanceof EntityPlayer)) {
-      if (entity instanceof EntityLiving) {
-        this.renderMobESP(entity, event.getX(), event.getY(),
-                event.getZ(), event.getRenderPartialTicks());
-      }
-    }
-  }
-
-  private void renderMobESP(Entity entityIn, double x, double y, double z,
-                            float partialTicks) {
-
-    if (entityIn != Wrapper.getPlayer() && Wrapper.getWorld() != null) {
+    if (!(entity instanceof EntityPlayer) && entity instanceof EntityLiving) {
       GL11.glPushMatrix();
       GL11Helper.enableDefaults();
-      GL11.glEnable(GL11.GL_LINE_SMOOTH);
-      GL11.glEnable(GL11.GL_BLEND);
-      GL11.glDisable(GL11.GL_DEPTH_TEST);
-      GL11.glDisable(GL11.GL_LIGHTING);
-      GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-      GL11.glDepthMask(false);
       GlStateManager.depthMask(false);
       GlStateManager.disableTexture2D();
       GlStateManager.disableLighting();
@@ -55,13 +38,15 @@ public class ModuleMobESP extends Module {
       GlStateManager.disableBlend();
       GL11.glLineWidth(1.0F);
 
-      AxisAlignedBB var11 = entityIn.getEntityBoundingBox();
-      AxisAlignedBB var12 = new AxisAlignedBB(var11.minX - entityIn.posX
-              + x - 0.2D, var11.minY - entityIn.posY + y, var11.minZ
-              - entityIn.posZ + z - 0.2D, var11.maxX - entityIn.posX + x
-              + 0.2D, var11.maxY - entityIn.posY + y + 0.2D, var11.maxZ
-              - entityIn.posZ + z + 0.2D);
-
+      AxisAlignedBB var11 = entity.getEntityBoundingBox();
+      AxisAlignedBB var12 = new AxisAlignedBB(
+              var11.minX - entity.posX + event.getX() - 0.2D,
+              var11.minY - entity.posY + event.getY(),
+              var11.minZ - entity.posZ + event.getZ() - 0.2D,
+              var11.maxX - entity.posX + event.getX() + 0.2D,
+              var11.maxY - entity.posY + event.getY() + 0.2D,
+              var11.maxZ - entity.posZ + event.getZ() + 0.2D
+      );
       RenderGlobal.drawOutlinedBoundingBox(var12, 201, 4, 155, 255);
 
       GL11Helper.disableDefaults();
@@ -70,13 +55,8 @@ public class ModuleMobESP extends Module {
       GlStateManager.enableCull();
       GlStateManager.disableBlend();
       GlStateManager.depthMask(true);
-      GL11.glDepthMask(true);
-      GL11.glEnable(GL11.GL_DEPTH_TEST);
-      GL11.glDisable(GL11.GL_BLEND);
-      GL11.glEnable(GL11.GL_LIGHTING);
-      GL11.glDisable(GL11.GL_LINE_SMOOTH);
       GL11.glPopMatrix();
     }
-
   }
+
 }
--- src/minecraft/de/paxii/clarinet/module/render/ModulePlayerESP.java
+++ src/minecraft/de/paxii/clarinet/module/render/ModulePlayerESP.java
@@ -2,7 +2,7 @@ package de.paxii.clarinet.module.render;
 
 import de.paxii.clarinet.Wrapper;
 import de.paxii.clarinet.event.EventHandler;
-import de.paxii.clarinet.event.events.entity.PreRenderEntityEvent;
+import de.paxii.clarinet.event.events.entity.PostRenderEntityEvent;
 import de.paxii.clarinet.module.Module;
 import de.paxii.clarinet.module.ModuleCategory;
 import de.paxii.clarinet.util.render.GL11Helper;
@@ -16,6 +16,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import org.lwjgl.opengl.GL11;
 
 public class ModulePlayerESP extends Module {
+
   public ModulePlayerESP() {
     super("PlayerESP", ModuleCategory.RENDER, -1);
 
@@ -24,26 +25,12 @@ public class ModulePlayerESP extends Module {
   }
 
   @EventHandler
-  public void renderESP(PreRenderEntityEvent event) {
+  public void renderESP(PostRenderEntityEvent event) {
     Entity entity = event.getRenderedEntity();
 
     if (entity instanceof EntityPlayer && entity != Wrapper.getPlayer()) {
-      this.renderPlayerESP(entity, event.getX(), event.getY(),
-              event.getZ(), event.getRenderPartialTicks());
-    }
-  }
-
-  private void renderPlayerESP(Entity entityIn, double x, double y, double z,
-                               float partialTicks) {
-    if (Wrapper.getWorld() != null) {
       GL11.glPushMatrix();
       GL11Helper.enableDefaults();
-      GL11.glEnable(GL11.GL_LINE_SMOOTH);
-      GL11.glEnable(GL11.GL_BLEND);
-      GL11.glDisable(GL11.GL_DEPTH_TEST);
-      GL11.glDisable(GL11.GL_LIGHTING);
-      GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-      GL11.glDepthMask(false);
       GlStateManager.depthMask(false);
       GlStateManager.disableTexture2D();
       GlStateManager.disableLighting();
@@ -53,17 +40,19 @@ public class ModulePlayerESP extends Module {
 
       int boxColor = 0xFF0000;
       if (Wrapper.getFriendManager().isFriend(
-              entityIn.getName())) {
-        boxColor = Wrapper.getFriendManager().getFriendColor(entityIn.getName());
+              entity.getName())) {
+        boxColor = Wrapper.getFriendManager().getFriendColor(entity.getName());
       }
 
-      AxisAlignedBB var11 = entityIn.getEntityBoundingBox();
-      AxisAlignedBB var12 = new AxisAlignedBB(var11.minX - entityIn.posX
-              + x - 0.2D, var11.minY - entityIn.posY + y, var11.minZ
-              - entityIn.posZ + z - 0.2D, var11.maxX - entityIn.posX + x
-              + 0.2D, var11.maxY - entityIn.posY + y + 0.2D, var11.maxZ
-              - entityIn.posZ + z + 0.2D);
-
+      AxisAlignedBB var11 = entity.getEntityBoundingBox();
+      AxisAlignedBB var12 = new AxisAlignedBB(
+              var11.minX - entity.posX + event.getX() - 0.2D,
+              var11.minY - entity.posY + event.getY(),
+              var11.minZ - entity.posZ + event.getZ() - 0.2D,
+              var11.maxX - entity.posX + event.getX() + 0.2D,
+              var11.maxY - entity.posY + event.getY() + 0.2D,
+              var11.maxZ - entity.posZ + event.getZ() + 0.2D
+      );
       RenderGlobal.drawOutlinedBoundingBox(var12, boxColor);
 
       GL11Helper.disableDefaults();
@@ -72,12 +61,8 @@ public class ModulePlayerESP extends Module {
       GlStateManager.enableCull();
       GlStateManager.disableBlend();
       GlStateManager.depthMask(true);
-      GL11.glDepthMask(true);
-      GL11.glEnable(GL11.GL_DEPTH_TEST);
-      GL11.glDisable(GL11.GL_BLEND);
-      GL11.glEnable(GL11.GL_LIGHTING);
-      GL11.glDisable(GL11.GL_LINE_SMOOTH);
       GL11.glPopMatrix();
     }
   }
+
 }
