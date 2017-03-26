f213236b3f2abac286c3ffc87f3fa62e996d75bf
Paxi <dev@paxii.de>
2017-03-26 20:55:03 +0200

    [BUGFIX] Make AutoFish sound based

--- src/minecraft/de/paxii/clarinet/module/player/ModuleAutoFish.java
+++ src/minecraft/de/paxii/clarinet/module/player/ModuleAutoFish.java
@@ -6,51 +6,64 @@ import de.paxii.clarinet.event.events.entity.EntityVelocityEvent;
 import de.paxii.clarinet.module.Module;
 import de.paxii.clarinet.module.ModuleCategory;
 
+import net.minecraft.client.audio.ISoundEventListener;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.EntityFishHook;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
 import net.minecraft.util.EnumHand;
 
 public class ModuleAutoFish extends Module {
-  private ThreadAutoFish autoFishThread;
+
+  private boolean fishSound;
+  private ISoundEventListener soundEventListener;
 
   public ModuleAutoFish() {
     super("AutoFish", ModuleCategory.PLAYER, -1);
 
-    this.autoFishThread = new ThreadAutoFish();
     this.setRegistered(true);
-    this.setDescription("Autmatically fishes for you.");
+    this.setDescription("Automatically fishes for you.");
+    this.soundEventListener = (soundIn, accessor) -> {
+      if (soundIn.getSound().getSoundLocation().getResourcePath().equals("random/splash")) {
+        this.fishSound = true;
+      }
+    };
+  }
+
+  @Override
+  public void onEnable() {
+    Wrapper.getMinecraft().getSoundHandler().getSndManager().addListener(this.soundEventListener);
   }
 
   @EventHandler
   public void onEntityVelocity(EntityVelocityEvent event) {
-    Entity e = Wrapper.getWorld().getEntityByID(event.getEntityID());
+    Entity entity = Wrapper.getWorld().getEntityByID(event.getEntityID());
 
-    if (e instanceof EntityFishHook) {
-      EntityFishHook fishHook = (EntityFishHook) e;
+    if (entity instanceof EntityFishHook) {
+      EntityFishHook fishHook = (EntityFishHook) entity;
 
-      if (fishHook.getAngler().getEntityId() == Wrapper.getPlayer().getEntityId()) {
-        if (event.getVelocityPacket().getMotionX() == 0
-                && event.getVelocityPacket().getMotionY() < -1000
-                && event.getVelocityPacket().getMotionZ() == 0) {
-          new Thread(this.autoFishThread).start();
-        }
+      if (this.fishSound
+              && fishHook.getAngler().getEntityId() == Wrapper.getPlayer().getEntityId()
+              && event.getVelocityPacket().getMotionX() == 0
+              && event.getVelocityPacket().getMotionZ() == 0) {
+        new Thread(() -> {
+          for (int i = 0; i < 2; i++) {
+            try {
+              Thread.sleep(200);
+            } catch (InterruptedException exception) {
+              exception.printStackTrace();
+            }
+
+            Wrapper.getSendQueue().addToSendQueue(new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND));
+          }
+        }).start();
+        this.fishSound = false;
       }
     }
   }
 
-  class ThreadAutoFish implements Runnable {
-    @Override
-    public void run() {
-      for (int i = 0; i < 2; i++) {
-        try {
-          Thread.sleep(200);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-
-        Wrapper.getSendQueue().addToSendQueue(new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND));
-      }
-    }
+  @Override
+  public void onDisable() {
+    Wrapper.getMinecraft().getSoundHandler().getSndManager().removeListener(this.soundEventListener);
   }
+
 }
