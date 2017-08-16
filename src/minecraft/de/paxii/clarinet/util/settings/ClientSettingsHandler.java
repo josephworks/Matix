dd6eede5264a8853dc6950bf492e5ac5cf6e1985
Paxi <dev@paxii.de>
2017-08-16 23:40:23 +0200

    [BUGFIX] Load ClientSettings on startup without Event

--- src/minecraft/de/paxii/clarinet/util/settings/ClientSettingsHandler.java
+++ src/minecraft/de/paxii/clarinet/util/settings/ClientSettingsHandler.java
@@ -1,54 +1,35 @@
 package de.paxii.clarinet.util.settings;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import de.paxii.clarinet.Wrapper;
 import de.paxii.clarinet.event.EventHandler;
 import de.paxii.clarinet.event.EventManager;
-import de.paxii.clarinet.event.events.game.StartGameEvent;
 import de.paxii.clarinet.event.events.game.StopGameEvent;
+import de.paxii.clarinet.util.file.FileService;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
 
 public class ClientSettingsHandler {
   public ClientSettingsHandler() {
     Wrapper.getEventManager().register(this);
 
+    this.onStartGame();
     Runtime.getRuntime().addShutdownHook(new Thread(() -> EventManager.call(new StopGameEvent())));
   }
 
-  @EventHandler
-  public void onStartGame(StartGameEvent event) {
+  public void onStartGame() {
     File settingsFile = new File((ClientSettings.getClientFolderPath().getValue()), "/settings.json");
 
     try {
-      Gson gson = new Gson();
-
-      if (settingsFile.exists()) {
-        BufferedReader br = new BufferedReader(new FileReader(settingsFile));
-        String line, jsonString = "";
-
-        while ((line = br.readLine()) != null) {
-          jsonString += line;
-        }
-
-        ClientSettingsContainer clientSettingsContainer = gson.fromJson(jsonString, ClientSettingsContainer.class);
-
-        if (clientSettingsContainer != null) {
-          clientSettingsContainer.getClientSettings().forEach((k, v) -> {
-            ClientSetting clientSetting = new ClientSetting<>(k, v);
-            clientSetting = ClientSettingValueManager.patchValue(clientSetting);
-            ClientSettings.getClientSettings().put(k, clientSetting);
-          });
-        }
-
-        br.close();
+      ClientSettingsContainer clientSettingsContainer = FileService.getFileContents(settingsFile, ClientSettingsContainer.class);
+      if (clientSettingsContainer != null) {
+        clientSettingsContainer.getClientSettings().forEach((k, v) -> {
+          ClientSetting clientSetting = new ClientSetting<>(k, v);
+          clientSetting = ClientSettingValueManager.patchValue(clientSetting);
+          ClientSettings.getClientSettings().put(k, clientSetting);
+        });
       }
-    } catch (Exception e) {
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
@@ -56,15 +37,9 @@ public class ClientSettingsHandler {
   @EventHandler
   public void onStopGame(StopGameEvent event) {
     try {
-      Gson gson = new GsonBuilder().setPrettyPrinting().create();
       File settingsFile = new File((ClientSettings.getClientFolderPath().getValue()), "/settings.json");
-      settingsFile.createNewFile();
-      FileWriter fileWriter = new FileWriter(settingsFile);
-
-      String jsonString = gson.toJson(ClientSettingsContainer.buildContainer(ClientSettings.getClientSettings()));
-      fileWriter.write(jsonString);
-      fileWriter.close();
-    } catch (Exception e) {
+      FileService.setFileContentsAsJson(settingsFile, ClientSettingsContainer.buildContainer(ClientSettings.getClientSettings()));
+    } catch (IOException e) {
       e.printStackTrace();
     }
 
